4448c9386c50c16f6d86862c6dad0ed4217eaa9f
angelbcdev <albertobernechea@gmail.com>
2025-09-24 13:17:02 -0400

    ch

---
+++ src/data/floor/matrixForFloor.ts
@@ -0,0 +1,35 @@
+export const matrix = [
+  [
+    { side: 0, ocupated: false, effect: "none", isAttack: false },
+    { side: 0, ocupated: false, effect: "none" },
+    { side: 0, ocupated: false, effect: "none" },
+    { side: 1, ocupated: false, effect: "none" },
+    { side: 1, ocupated: false, effect: "none" },
+    { side: 1, ocupated: false, effect: "none" },
+  ],
+  [
+    { side: 0, ocupated: false, effect: "none", isAttack: false },
+    { side: 0, ocupated: false, effect: "none", isAttack: false },
+    { side: 0, ocupated: false, effect: "none" },
+    { side: 1, ocupated: false, effect: "none" },
+    { side: 1, ocupated: false, effect: "none" },
+    { side: 1, ocupated: false, effect: "none" },
+  ],
+  [
+    { side: 0, ocupated: false, effect: "none", isAttack: false },
+    { side: 0, ocupated: false, effect: "none" },
+    { side: 0, ocupated: false, effect: "none" },
+    { side: 1, ocupated: false, effect: "none", isVisible: false },
+    { side: 1, ocupated: false, effect: "none" },
+    { side: 1, ocupated: false, effect: "none" },
+  ],
+  // this is the last row 4 x 3
+  // [
+  //   { side: 0, ocupated: false, effect: "none", isAttack: false },
+  //   { side: 0, ocupated: false, effect: "none", isAttack: false },
+  //   { side: 0, ocupated: false, effect: "none" },
+  //   { side: 1, ocupated: false, effect: "none" },
+  //   { side: 1, ocupated: false, effect: "none" },
+  //   { side: 1, ocupated: false, effect: "none" },
+  // ],
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+<<<<<<< Updated upstream
 import Path from "path";
+=======
+import path from "path";
+>>>>>>> Stashed changes
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< Updated upstream
   base: "./",
   resolve: {
     alias: {
       "@": Path.resolve(__dirname, "src"),
     },
   },
+=======
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "./",
+>>>>>>> Stashed changes
 });
